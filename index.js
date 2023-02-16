c85f5204f739921df57561cae2e8054d07364182
robiulXhasan <robi16275@gmail.com>
2023-02-16 23:00:38 +0600

    comment added

--- index.js
+++ index.js
@@ -1,3 +1,4 @@
+// get random number pin
 function getPin() {
   const pin = pinGenerator();
   const pinString = pin + "";
@@ -7,25 +8,28 @@ function getPin() {
     getPin();
   }
 }
-
+// get random number
 function pinGenerator() {
   const random = Math.round(Math.random() * 10000);
   return random;
 }
 
+// [pin generator button click handling]
 document.getElementById("pin-generator-button").addEventListener("click", function () {
   const pin = getPin();
   document.getElementById("display-pin").value = pin;
 });
 
+// keypad click
 function clickNumber(number) {
   const inputPin = parseInt(document.getElementById("input-pin-display").value + number);
   document.getElementById("input-pin-display").value = inputPin;
 }
+// clear input display
 document.getElementById("clear-btn").addEventListener("click", function () {
   document.getElementById("input-pin-display").value = "";
 });
-
+// check matched or not
 document.getElementById("submit").addEventListener("click", function () {
   const inputPin = parseInt(document.getElementById("input-pin-display").value);
   const generatedPin = parseInt(document.getElementById("display-pin").value);
